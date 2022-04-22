e94111cd4245604da6e1811198b581b8a408ce90
jboyd <jab7bp@virginia.edu>
2022-04-21 23:47:18 -0400

    added apv_adc_ratios

--- adc_ratios.C
+++ adc_ratios.C
@@ -11,11 +11,11 @@
 #include "./include/search_file.C"
 
 //Define some global variables
-int const ADC_cut = 700;
+int const ADC_cut = 300;
 int apv_chan_adc[128][2];
 int apv_strip_adc[128][2];
-int nstrips = 3840;
-Int_t max_strips = 8000;
+Int_t max_strips = (3840);
+Int_t max_events = 100;
 Int_t nAPV_strips = 128;
 Int_t gaus_min_bin;
 Int_t gaus_max_bin;
@@ -36,44 +36,30 @@ TF1 *xtalk_fit;
 // 	tpt_db_cuts->AddText(db_cuts[cut_cnt]);
 // }
 
-void xtalk(int runnum = 12060){ 
+void adc_ratios(int runnum = 13560){ 
 	TChain *TC = new TChain("T");
 	// const char * DATA_DIR = Form("/lustre19/expphy/volatile/halla/sbs/jboyd/Rootfiles/xtalk/%i/", runnum);
-	// const char * DATA_DIR = Form("/lustre19/expphy/volatile/halla/sbs/jboyd/Rootfiles/xtalk/%i/", runnum);
-	const char * DATA_DIR = "/lustre19/expphy/volatile/halla/sbs/jboyd/Rootfiles/xtalk/";
-	// const char * protorootfile = Form("e1209019_fullreplay_%i_stream0_seg0_0.root", runnum);
-	// const char * protorootfile = Form("e1209019_replayed_%i.root", runnum);
+	const char * DATA_DIR = "/lustre19/expphy/volatile/halla/sbs/jboyd/Rootfiles/xtalk/before_cuts/";
+	const char * protorootfile = Form("e1209019_replayed_%i.root", runnum);
+	// const char * protorootfile = Form("e1209019_fullreplay_%i_*", runnum);
 	// const char * protorootfile = Form("e1209019_replayed_%i_%s.root", runnum, cut);
-	const char * protorootfile = Form("e1209019_fullreplay_%i_*", runnum);
+	// const char * protorootfile = "e1209019_fullreplay_13656_stream0_seg9_9.root";
 	// const char * protorootfile = Form("*%i*%s*", runnum, cut);
-	// const char * protorootfile = "*";
+	// const char * protorootfile = Form("*%i*", runnum);
 
-	// TString rootfile = Form("%s%s", DATA_DIR, protorootfile);
-	// cout << "Input rootfile is: " << rootfile << endl;
-	// TC->Add( rootfile );
-
-	for(int seg=0; seg < 10; seg++){
-		protorootfile = Form("/%i/e1209019_replayed_%i.root", runnum, runnum);
-		TString rootfile = Form("%s%s", DATA_DIR, protorootfile);
-		TC->Add( rootfile );
-		
-	}
+	TString rootfile = Form("%s%s", DATA_DIR, protorootfile);
+	cout << "Input rootfile is: " << rootfile << endl;
+	TC->Add( rootfile );
 
 	// Loop for Chain
 	// const char * protorootfile;
-	// for(int seg=2; seg <10; seg++){
-	// 	protorootfile = Form("e1209019_fullreplay_12060_stream0_seg%i_%i_1.root", seg, seg);
+	// for(int seg=0; seg <11; seg++){
+	// 	protorootfile = Form("e1209019_fullreplay_11562_stream0_seg%i_%i.root", seg, seg);
 	// 	TString rootfile = Form("%s%s", DATA_DIR, protorootfile);
 	// 	cout << "Input rootfile is: " << rootfile << endl;
 	// 	TC->Add( rootfile );
 	// }
 	
-
-	//Define an output file to print things protorootfile
-	// ofstream myfile;
-	// myfile.open("output.txt");
-	// myfile << "occupancy: \n \n";
-	// myfile << "-------------------------" << endl;
 
 	//Cuts from db_bb.gem.dat
 	// TString db_cuts[] = {
@@ -172,113 +158,63 @@ void xtalk(int runnum = 12060){
 	//where that main loop starts.
 	int nAPVs = 30;
 	int occupancy_bin_num = 10000;
-	int first_apv = 10;
-	int last_apv = 20;
+	int first_apv = 1;
+	int last_apv = 10;
 	
-	double apv_occupancy_mean[nAPVs];
-	//LABELS and Lines
-	TPaveLabel *tpl_apv_occupancy_mean_U[nAPVs];
-	TLine *tl_apv_occupancy_lines[nAPVs];
-
-	//Histogram with all individual APV occupancies
-	TH1D *h_all_APV_occupancies_U = new TH1D("h_all_APV_occupancies_U", "", (occupancy_bin_num*nAPVs), 0.0, double(nAPVs));
 
 	//Grab some maximum values to use as limits and constraints
-	int first_occupancy_bin = first_apv*occupancy_bin_num;
 	double max_U_ADC = TC->GetMaximum("bb.gem.hit.ADCmaxsampU");
 	double max_V_ADC = TC->GetMaximum("bb.gem.hit.ADCmaxsampV");
 
 	//Define 1-D Histograms
 	TH1D *h_APV_U_ADCmax_chan_ratios[nAPVs];
-	TH1D *h_APV_occupancy_U[nAPVs];
+
 
 	//Define 2-D Histograms
-	TH2D *h2_ADCmax = new TH2D("h2_ADCsamples", "", nstrips, 0, nstrips, nstrips, 0, nstrips);
-	TH2D *h2_U_ADCmax = new TH2D("h2_U_ADCmax", "", nstrips, 0, nstrips, max_U_ADC+50, 0, max_U_ADC+50);
+	TH2D *h2_ADCmax = new TH2D("h2_ADCsamples", "", max_strips, 0, max_strips, 2, 0, max_strips);
+	TH2D *h2_U_ADCmax = new TH2D("h2_U_ADCmax", "", max_strips, 0, max_strips, max_U_ADC+50, 0, max_U_ADC+50);
 
 	//This is a general histogram that will hold all the histograms for all of the APVs.
 	TH2D *h_apv_ADCmax_istrip[nAPVs];
 	TH2D *h2_APV_U_ADCmax_apvstrip[nAPVs];
 	TH2D *h2_APV_U_ADCmax_apvchan[nAPVs];
-	TH2D *h2_APV_U_ADCmax_neigh_chan[nAPVs];
-	TH2D *h2_APV_U_ADCmax_neigh_chan_lrg_sml[nAPVs];
+
 
 //************************************************************//
 //******************      MAIN APV LOOP     ******************//	
 //************************************************************//
 	cout << endl << "Run number: " << runnum << endl;
 	cout << endl << "Calculating total occupancy on layer." << endl;
-	
-	TH1D *h_total_occupancy = new TH1D("h_total_occupancy_all_strips", "", occupancy_bin_num, 0, 1);
-	TH1D *h_total_occupancy_U = new TH1D("h_total_occupancy_U", "", occupancy_bin_num, 0, 1);
+
 	int total_event_num = 0;
-	double total_occupancy = 0;
-	double total_occupancy_U = 0;
-	while(TC->GetEntry(total_event_num++)){
-		TC->GetEntry(total_event_num);
-		total_occupancy = nstrips_keep/(2*nstrips);
-		total_occupancy_U = nstrips_keepU/nstrips;
-		h_total_occupancy->Fill(total_occupancy);
-		h_total_occupancy_U->Fill(total_occupancy_U);
-	}
-
-	double last_occupancy_bin = (h_total_occupancy->FindLastBinAbove(0, 1) + 500)/double(occupancy_bin_num);
-	double last_occupancy_bin_U = (h_total_occupancy_U->FindLastBinAbove(0, 1) + 500)/double(occupancy_bin_num);
-	cout << "Finished calculating all total occupancies." << endl;
-
-	TCanvas *c_total_occupancy = new TCanvas("c_total_occupancy", "", 700, 500);
-	h_total_occupancy->Draw();
-	h_total_occupancy->SetTitle(Form("Total Occupancy On GEM, All strips, All events - Run: %i", runnum));
-	h_total_occupancy->GetXaxis()->SetTitle("Occupancy");
-	h_total_occupancy->SetAxisRange(0, last_occupancy_bin, "X");
-	h_total_occupancy->GetYaxis()->SetTitle("Entries");
-	// tpt_db_cuts->Draw();
-	c_total_occupancy->Update();
-	c_total_occupancy->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/total_occupancy_%i.pdf", runnum));
-
-
-	TCanvas *c_total_occupancy_U = new TCanvas("c_total_occupancy_U", "", 700, 500);
-	h_total_occupancy_U->Draw();
-	h_total_occupancy_U->SetTitle(Form("Total Occupancy On GEM U Strips, ALL Events - Run: %i", runnum));
-	h_total_occupancy_U->GetXaxis()->SetTitle("Occupancy");
-	h_total_occupancy_U->SetAxisRange(0, last_occupancy_bin_U, "X");
-	h_total_occupancy_U->GetYaxis()->SetTitle("Entries");
-	c_total_occupancy_U->Update();
-	c_total_occupancy_U->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/total_occupancy_U_%i.pdf", runnum));
 	
 	cout << "Analyzing data for APVs " << first_apv << " through " << last_apv << "." << endl;
 
 	for(int apv_cnt = first_apv; apv_cnt < last_apv; apv_cnt++) {
 		cout << "Processing data for apv: " << apv_cnt << endl;
-
 		int min_APV_strip = APV_strip_nums(apv_cnt, "min");
 		int max_APV_strip = APV_strip_nums(apv_cnt, "max");
 
 		h_APV_U_ADCmax_chan_ratios[apv_cnt] = new TH1D(Form("h_APV%i_U_ADCmax_chan_ratios", apv_cnt), "", 300, 0, 30);
-		h_APV_occupancy_U[apv_cnt] = new TH1D(Form("h_APV%i_occupancy_U", apv_cnt), "", occupancy_bin_num, 0.0, 1);
 		h_apv_ADCmax_istrip[apv_cnt] = new TH2D(Form("h2_APV%i_U_ADCmax_istrip",apv_cnt), "",128, APV_strip_nums(apv_cnt, "min"), APV_strip_nums(apv_cnt, "max"), max_U_ADC, 0, max_U_ADC );
 		h2_APV_U_ADCmax_apvstrip[apv_cnt] = new TH2D(Form("h2_APV%i_U_ADCmax_apvstrip", apv_cnt), "",128, 0, 128, max_U_ADC, 0, max_U_ADC );
 		h2_APV_U_ADCmax_apvchan[apv_cnt] = new TH2D(Form("h2_APV%i_U_ADCmax_apvchan", apv_cnt), "", 128, 0, 128, max_U_ADC, 0, max_U_ADC );
-		h2_APV_U_ADCmax_neigh_chan[apv_cnt] = new TH2D(Form("h2_APV%i_U_ADCmax_neigh_chan", apv_cnt), "", max_U_ADC, 0, max_U_ADC,max_U_ADC,  0, max_U_ADC);
-		h2_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt] = new TH2D(Form("h2_APV%i_U_ADCmax_neigh_chan_lrg_sml", apv_cnt), "", max_U_ADC, 0, max_U_ADC,max_U_ADC,  0, max_U_ADC);
+
+
 
 		//We can either loop over all events or select some events:
 		//Loop over all events:
-		int apv_event_num = 0;
-		while(TC->GetEntry(apv_event_num++)){
-			TC->GetEntry(apv_event_num);
-
-		if(apv_event_num%10000 == 0){cout << "Analyzing event " << apv_event_num << " of " << TC->GetEntries() << " total events." << endl;}
-		// //Loop over selected events:
-		// for(int evt = 5; evt <= 5; evt++){
-		// 	cout << "ndata u: " << Ndata_IsU << endl;
-		// 	TC->GetEntry(evt);
+		// int apv_event_num = 0;
+		// while(TC->GetEntry(apv_event_num++)){
+		// 	TC->GetEntry(apv_event_num);
 
 		
+		//Loop over selected events:
+		for(int evt = 5; evt <= 60000; evt++){
+			TC->GetEntry(evt);
 			int occupancy_cnt = 0;
 
 			//Only look at U-strips and loop over them. We only look up to the number of U-strips fired (Ndata_IsU)
-
 			for(int i = 0; i < Ndata_IsU; i++){
 				h2_ADCmax->Fill(istrip[i], ADC_max[i]);
 				
@@ -306,15 +242,9 @@ void xtalk(int runnum = 12060){
 					}
 
 				}
-				//Make occupancy plots for single APVs:
-				// if(IsU[i] && (istrip[i] >= APV_strip_nums(apv_cnt, "min")) && (istrip[i] <= APV_strip_nums(apv_cnt, "max")) && ADC_max
+		
+			}
 
-				
-			}
-			// cout << "occupancy after event: " << evt << "   " << occupancy_cnt <<  "  & occupancy = " << double(occupancy_cnt)/nAPV_strips << endl;
-			if(build_APV_occupancy_U){
-				if(double(occupancy_cnt)/nAPV_strips > 0.0){h_APV_occupancy_U[apv_cnt]->Fill(double(occupancy_cnt)/nAPV_strips);}
-			}
 
 			//The histograms for this event have been filled.
 			//SCAN THROUGH FILLED HISTOGRAMS
@@ -336,15 +266,14 @@ void xtalk(int runnum = 12060){
 					if(chan_n_adc > chan_n_plus_1_ADC && chan_n_plus_1_ADC != 0 && chan_n_adc > ADC_cut){
 						double ADC_ratio = chan_n_adc/chan_n_plus_1_ADC;
 						h_APV_U_ADCmax_chan_ratios[apv_cnt]->Fill(ADC_ratio);
-						// h2_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt]->Fill(chan_n_adc, chan_n_plus_1_ADC);
+
 					}
 					else if(chan_n_plus_1_ADC > chan_n_adc && chan_n_adc != 0 && chan_n_plus_1_ADC > ADC_cut){
 						double ADC_ratio = chan_n_plus_1_ADC/chan_n_adc;
 						h_APV_U_ADCmax_chan_ratios[apv_cnt]->Fill(ADC_ratio);
-						// h2_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt]->Fill(chan_n_plus_1_ADC, chan_n_adc);
+
 					}
-					//Fill channel n on the y-axis. Fill channel n+1 on the x-axis. 
-					// h2_APV_U_ADCmax_neigh_chan[apv_cnt]->Fill(chan_n_plus_1_ADC, chan_n_adc);
+
 				}
 			}
 		//End of looping through events
@@ -359,9 +288,6 @@ void xtalk(int runnum = 12060){
 		double apv_strip_adc_x[128];
 		double apv_strip_adc_y[128];
 
-		if(build_APV_occupancy_U){
-			apv_occupancy_mean[apv_cnt]=h_APV_occupancy_U[apv_cnt]->GetMean();
-		}
 
 		for(int i=0; i<128; i++){
 			apv_chan_adc_x[i] = i;
@@ -488,99 +414,15 @@ void xtalk(int runnum = 12060){
 			if(apv_cnt == (last_apv - 1)){
 				c_adcmax_ratios_apv[apv_cnt]->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/APV_ratios_U_strips_all_%i_ADCcut_%i.pdf)", runnum, ADC_cut));
 			}
-			c_adcmax_ratios_apv[apv_cnt]->Close();
-			gSystem->ProcessEvents();
+			// c_adcmax_ratios_apv[apv_cnt]->Close();
+			// gSystem->ProcessEvents();
 		}
-		
-		
-		// TCanvas *c_APV_U_ADCmax_neigh_chan[nAPVs];
-		// c_APV_U_ADCmax_neigh_chan[apv_cnt] = new TCanvas(Form("APV%i, ADCmax on strip n vs ADCmax on strip n + 1", apv_cnt), Form("APV%i n and n+1", apv_cnt), 600, 500);
-		// h2_APV_U_ADCmax_neigh_chan[apv_cnt]->GetZaxis()->SetRangeUser(10,2000);
-		// h2_APV_U_ADCmax_neigh_chan[apv_cnt]->Draw("colz");
-		// h2_APV_U_ADCmax_neigh_chan[apv_cnt]->SetTitle(Form("ADCmax on strip n vs ADCmax on strip n+1 (Run: %d)", runnum));
-		// h2_APV_U_ADCmax_neigh_chan[apv_cnt]->GetXaxis()->SetTitle("ADCmax on strip (n+1)");
-		// h2_APV_U_ADCmax_neigh_chan[apv_cnt]->GetYaxis()->SetTitle("ADCmax on strip n");
-		// c_APV_U_ADCmax_neigh_chan[apv_cnt]->Update();
-
-		// TCanvas *c_APV_U_ADCmax_neigh_chan_lrg_sml[nAPVs];
-		// c_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt] = new TCanvas(Form("APV%i, ADCmax: Larger of 2 strips on x, smaller on y", apv_cnt), "", 600, 500);
-		// h2_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt]->GetZaxis()->SetRangeUser(0,3000);
-		// h2_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt]->Draw("colz");
-		// h2_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt]->SetTitle(Form("ADCmax for n and (n+1): Larger ADC on x-axis; Smaller on y-axis. (Run: %d)", runnum));
-		// h2_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt]->GetXaxis()->SetTitle("ADCmax of larger ADC");
-		// h2_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt]->GetYaxis()->SetTitle("ADCmax of smaller ADC");
-		// c_APV_U_ADCmax_neigh_chan_lrg_sml[apv_cnt]->Update();
-		
-		//SAVE APV OCCUPANCY PLOTS TO PDF
-		if(build_APV_occupancy_U){
-			double last_APV_occupancy_bin_U = (h_APV_occupancy_U[apv_cnt]->FindLastBinAbove(0, 1) + 500)/double(occupancy_bin_num);
-
-		
-			TCanvas *c_APV_occupancy_U[nAPVs];
-			c_APV_occupancy_U[apv_cnt] = new TCanvas(Form("c_APV%i_occupancy_U", apv_cnt), "", 700, 500);
-			h_APV_occupancy_U[apv_cnt]->Draw();
-				h_APV_occupancy_U[apv_cnt]->SetTitle(Form("Occupancy on APV %i - Run: %i - Ustrips Only", apv_cnt, runnum));
-				h_APV_occupancy_U[apv_cnt]->GetXaxis()->SetTitle("Occupancy");
-				h_APV_occupancy_U[apv_cnt]->GetYaxis()->SetTitle("Entries");
-				h_APV_occupancy_U[apv_cnt]->SetAxisRange(0, last_APV_occupancy_bin_U, "X");
-				c_APV_occupancy_U[apv_cnt]->Update();
-			if(apv_cnt == first_apv){
-				c_APV_occupancy_U[apv_cnt]->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/APV_occupancy_U_%i.pdf(", runnum));
-				// c_APV_occupancy_U[apv_cnt]->Clear();
-			}
-			else if (apv_cnt > first_apv && apv_cnt < (last_apv-1)){
-				c_APV_occupancy_U[apv_cnt]->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/APV_occupancy_U_%i.pdf", runnum));
-				// c_APV_occupancy_U[apv_cnt]->Clear();
-			}
-			if(apv_cnt == (last_apv - 1)){
-				c_APV_occupancy_U[apv_cnt]->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/APV_occupancy_U_%i.pdf)", runnum));
-				
-			}
-			c_APV_occupancy_U[apv_cnt]->Close();
-			gSystem->ProcessEvents();
-		}
-
-		//Put each APV occupancy histogram into the histogram for all APV h_all_APV_occupancies_U
-		int apv_occupancy_bin = 0;
-		for(int bin_cnt = first_occupancy_bin; bin_cnt < (first_occupancy_bin + occupancy_bin_num); bin_cnt ++){
-			h_all_APV_occupancies_U->SetBinContent(bin_cnt, h_APV_occupancy_U[apv_cnt]->GetBinContent(apv_occupancy_bin));
-			apv_occupancy_bin++;
-		}
-		
-		first_occupancy_bin += occupancy_bin_num;
-		
-	// cout<< "mean: " << h_APV_occupancy_U[13]->GetMean();
-	
 	
 		
 	//END OF APV_CNT LOOP
 	}
 	
-	if(build_APV_occupancy_U){
-		TCanvas *c_all_APV_occupancies_U = new TCanvas("c_all_APV_occupancies_U", "", 700, 500);
-		h_all_APV_occupancies_U->Draw("same");
-		h_all_APV_occupancies_U->SetStats(0);
-		h_all_APV_occupancies_U->SetTitle(Form("Occupancies on individual APVs - Run: %i - Ustrips only", runnum));
-		h_all_APV_occupancies_U->GetXaxis()->SetTitle("APVs and respective occupany bins (0.0 - 0.30)");
-		h_all_APV_occupancies_U->GetYaxis()->SetTitle("Entries");
-		c_all_APV_occupancies_U->Update();
-		cout << "h_apv_occ_u: " << (h_all_APV_occupancies_U->GetMaximum())*(2.0/3.0) << endl;
-		for(int apv = first_apv; apv < last_apv; apv++){
-			tpl_apv_occupancy_mean_U[apv] = new TPaveLabel(apv +.15, (h_all_APV_occupancies_U->GetMaximum())*(2.0/3.0), apv +.85, (h_all_APV_occupancies_U->GetMaximum()), Form("APV %i, Mean = %0.3f", apv, h_APV_occupancy_U[apv]->GetMean()));
-			// tpl_apv_occupancy_mean_U[apv] = new TPaveLabel(apv +.15, 140, apv +.85, 230, Form("APV %i, Mean = %0.3f", apv, h_APV_occupancy_U[apv]->GetMean()));
-			tpl_apv_occupancy_mean_U[apv]->SetBorderSize(1);
-			tpl_apv_occupancy_mean_U[apv]->SetTextAngle(90.);
-			tpl_apv_occupancy_mean_U[apv]->SetTextFont(53);
-			tpl_apv_occupancy_mean_U[apv]->SetTextSize(12);
-			tpl_apv_occupancy_mean_U[apv]->Draw();
-			tl_apv_occupancy_lines[apv] = new TLine(apv, 0, apv, (h_all_APV_occupancies_U->GetMaximum())*(1.05));
-			tl_apv_occupancy_lines[apv]->SetLineStyle(4);
-			tl_apv_occupancy_lines[apv]->Draw();
-			c_all_APV_occupancies_U->Update();
-		}
 
-		c_all_APV_occupancies_U->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/APV_all_occupancies_%i.pdf", runnum));
-	}
 ///////////////////
 //END OF MAIN LOOP
 /////////////////////////////////////////////
---
+++ apv_adc_ratios.C
@@ -0,0 +1,272 @@
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <iomanip>
+#include <math.h>
+#include <algorithm>
+#include <string>
+
+#include "./include/include_files.h"
+#include "./include/APV_strips.h"
+#include "./include/search_file.C"
+
+//Define some global variables
+int const ADC_cut = 700;
+int apv_chan_adc[128][2];
+int apv_strip_adc[128][2];
+
+int nAPVs = 30;
+int occupancy_bin_num = 10000;
+int first_apv = 10;
+int last_apv = 15;
+int ratio_bins = 300;
+
+//Set defaults for ADC on channel n and n+1
+double chan_n_adc = 0.0;
+double chan_n_plus_1_ADC = 0.0;
+double ADC_ratio = 0.0;
+
+int nstrips = 3840;
+Int_t max_strips = 8000;
+Int_t nAPV_strips = 128;
+Int_t gaus_min_bin;
+Int_t gaus_max_bin;
+
+
+
+
+void apv_adc_ratios(int runnum = 13444){
+	TChain *TC = new TChain("T");
+
+	const char * DATA_DIR =  "/lustre19/expphy/volatile/halla/sbs/jboyd/Rootfiles/xtalk";
+	// const char * DATA_DIR = "/lustre19/expphy/volatile/halla/sbs/puckett/GMN_REPLAYS/rootfiles";
+	const char * protorootfile;
+
+
+	// protorootfile = Form("/before_cuts/e1209019_replayed_%i.root", runnum);
+	// TString rootfile = Form("%s%s", DATA_DIR, protorootfile);
+	// cout << "Input root file is: " << rootfile << endl;
+	// TC->Add( rootfile );
+
+	//Loop for TChain
+	for(int seg=0; seg < 10; seg++){
+		protorootfile = Form("/%i/e1209019_replayed_%i.root", runnum, runnum);
+		TString rootfile = Form("%s%s", DATA_DIR, protorootfile);
+		TC->Add( rootfile );
+		
+	}
+
+	//CANVASES
+	TCanvas *c_APV_ratio_ADCmax_chan_U[nAPVs];
+
+	//Turn off all Branches to save memory/time
+	TC->SetBranchStatus("*", false);
+
+	//Turn on desired branches
+	
+	//Hit branches:
+	TC->SetBranchStatus("bb.gem.hit.nstripu", true);
+	TC->SetBranchStatus("bb.gem.hit.nstripv", true);
+	TC->SetBranchStatus("bb.gem.hit.ADCmaxsampU", true);
+	TC->SetBranchStatus("bb.gem.hit.ADCmaxsampV", true);
+	TC->SetBranchStatus("singletrack", true);
+
+	//Strip branches
+	// ****Ndata branches:
+	TC->SetBranchStatus("Ndata.bb.gem.m0.strip.ADCsamples", true);
+	TC->SetBranchStatus("Ndata.bb.gem.m0.strip.IsU", true);
+	TC->SetBranchStatus("Ndata.bb.gem.m0.strip.IsV", true);
+	TC->SetBranchStatus("bb.gem.m0.strip.nstrips_keepU", true);
+	//****Variable branches:
+	TC->SetBranchStatus("bb.gem.m0.strip.IsU", true);
+	TC->SetBranchStatus("bb.gem.m0.strip.IsV", true);
+	TC->SetBranchStatus("bb.gem.m0.strip.istrip", true);
+	TC->SetBranchStatus("bb.gem.m0.strip.ADCsamples", true);
+	TC->SetBranchStatus("bb.gem.m0.strip.ADCmax", true);
+	TC->SetBranchStatus("bb.gem.m0.strip.ADCsum", true);
+	TC->SetBranchStatus("bb.gem.m0.strip.nstrips_keep", true);
+	TC->SetBranchStatus("bb.gem.m0.strip.isampmax", true);
+
+
+	//Define variables that will hold the branch values/variables
+	//Ndata variables:
+	double singletrack;
+	Int_t Ndata_strip_ADC;
+	Int_t Ndata_IsU;
+	Int_t Ndata_IsV;
+	double nstrips_keepU;
+	double nstripu[max_strips];
+	double nstripv[max_strips];
+	double nstrips_keep;
+	
+	//Strip, ADC, etc, variable values:
+	Int_t isampmax[max_strips];
+	double IsU[max_strips];
+	double IsV[max_strips];
+	double istrip[max_strips];
+	double ADC_samples[max_strips];
+	double ADC_max[max_strips];
+	double ADC_sum[max_strips];
+
+
+	//Assign branches to these variables:
+	//Ndata
+	TC->SetBranchAddress("Ndata.bb.gem.m0.strip.ADCsamples", &Ndata_strip_ADC);
+	TC->SetBranchAddress("Ndata.bb.gem.m0.strip.IsU", &Ndata_IsU);
+	TC->SetBranchAddress("Ndata.bb.gem.m0.strip.IsV", &Ndata_IsV);
+
+	//Hits
+	TC->SetBranchAddress("bb.gem.hit.nstripu", &nstripu);
+	TC->SetBranchAddress("bb.gem.hit.nstripv", &nstripv);
+
+	//Strips
+	TC->SetBranchAddress("bb.gem.m0.strip.ADCsamples", &ADC_samples);
+	TC->SetBranchAddress("bb.gem.m0.strip.ADCmax", &ADC_max);
+	TC->SetBranchAddress("bb.gem.m0.strip.ADCsum", &ADC_sum);
+	TC->SetBranchAddress("bb.gem.m0.strip.IsU", &(IsU));
+	TC->SetBranchAddress("bb.gem.m0.strip.IsV", &(IsV));
+	TC->SetBranchAddress("bb.gem.m0.strip.istrip", &istrip);
+	TC->SetBranchAddress("bb.gem.m0.strip.isampmax", &isampmax);
+	TC->SetBranchAddress("bb.gem.m0.strip.nstrips_keep", &nstrips_keep);
+	TC->SetBranchAddress("bb.gem.m0.strip.nstrips_keepU", &nstrips_keepU);
+	TC->SetBranchAddress("singletrack", &singletrack);
+
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+//----------------------------     MAIN LOOP -------------------------------
+//                            Where the fun happens!!etc********************
+//This fist loop is to step through APVs. We will look at individual APVs and this is
+//where that main loop starts.
+
+	//Some general variables
+	double max_U_ADC = TC->GetMaximum("bb.gem.hit.ADCmaxsampU");
+	double max_V_ADC = TC->GetMaximum("bb.gem.hit.ADCmaxsampV");
+
+	//Define histograms:
+	//1-D histograms
+	TH1D *h_APV_ratio_ADCmax_chan_U[nAPVs];
+
+	//2-D histograms
+	TH2D *h2_ADCmax_U = new TH2D("h2_ADCmax_samples_U", "" , nstrips, 0, nstrips, nstrips, 0, nstrips);
+
+	cout << endl << "Run number: " << runnum << endl;
+	cout << endl << "Analyzing data for APVs " << first_apv << " up to " << last_apv << "." << endl << endl;
+
+//************************************************
+//-----------------APV LOOP-----------------------
+//************************************************
+
+	for(int apv_cnt = first_apv; apv_cnt < last_apv; apv_cnt++){
+		cout <<  "Processing data for apv: " << apv_cnt << endl << endl;
+		
+		//Max and min strips for the specific APV
+		int min_APV_strip = APV_strip_nums(apv_cnt, "min");
+		int max_APV_strip = APV_strip_nums(apv_cnt, "max");
+
+	//Create ratio histogram entry for each APV
+
+		h_APV_ratio_ADCmax_chan_U[apv_cnt] = new TH1D(Form("h_APV%i_ratio_ADCmax_chan_U", apv_cnt), "", ratio_bins, 0, nAPVs);
+	
+//EVENT //Loop all selected events:		
+		//We can either loop over all events or select some events:
+		//Loop over all events:
+
+		int apv_event_num = 0;
+		while(TC->GetEntry(apv_event_num++)){
+			if(apv_event_num%10000 == 0){cout << "Analyzing event " << apv_event_num << " of " << TC->GetEntries() << " total events." << endl;}
+			TC->GetEntry(apv_event_num);
+
+		
+//EVENT //Loop over selected events:
+
+		// for(int evt = 0; evt <= 50000; evt++){
+		
+		// 	// if(evt == 706 || evt == 1186) {continue;}
+		// 	cout << "evt: " << evt << endl;
+		// 	TC->GetEntry(evt);
+			
+
+			
+			//------------U-strips-------------
+			for(int i = 0; i < Ndata_IsU; i++){
+				
+				if(IsU[i]){
+					h2_ADCmax_U->Fill(istrip[i], ADC_max[i]);
+
+					//Only select strips that are in the selected range for the specific APV
+					//Need to cross reference the global strip numbers and relate them to those of the APV_strip_nums
+
+					if(istrip[i] >= min_APV_strip && istrip[i] <= max_APV_strip){
+						//Fill an array with the maximum ADC on each channel.
+						//Storing it in a 2D array to grab the maximum entry in each bin
+
+						//-----CHANNELS-------
+						apv_chan_adc[UV_APV_strip_to_channel(int(istrip[i])%128)][0] = UV_APV_strip_to_channel(int(istrip[i])%128);
+						apv_chan_adc[UV_APV_strip_to_channel(int(istrip[i])%128)][1] = ADC_max[i];
+
+						//-----STRIPS---------
+						apv_strip_adc[int(istrip[i])%128][0] = istrip[i];
+						apv_strip_adc[int(istrip[i])%128][1] = ADC_max[i];
+
+					}
+				
+				}
+
+			}
+			//End of looping through U-Strips
+
+			//----------RATIOS------------
+			for(int j = 0; j < 127; j++){
+				chan_n_adc = apv_chan_adc[j][1];
+				chan_n_plus_1_ADC = apv_chan_adc[j+1][1];
+				ADC_ratio = 0.0;
+				
+				//Always take larger ADC divided by smaller ADC for neighboring channels
+				if(chan_n_adc > chan_n_plus_1_ADC && chan_n_plus_1_ADC != 0 && chan_n_adc > ADC_cut){
+					double ADC_ratio = chan_n_adc/chan_n_plus_1_ADC;
+					h_APV_ratio_ADCmax_chan_U[apv_cnt]->Fill(ADC_ratio);
+				}
+				else if(chan_n_plus_1_ADC > chan_n_adc && chan_n_adc != 0 && chan_n_plus_1_ADC > ADC_cut){
+					double ADC_ratio = chan_n_plus_1_ADC/chan_n_adc;
+					h_APV_ratio_ADCmax_chan_U[apv_cnt]->Fill(ADC_ratio);
+				}
+
+	
+			}
+		
+		
+		}
+		//END of EVENTS
+
+		c_APV_ratio_ADCmax_chan_U[apv_cnt] = new TCanvas(Form("APV%i Ratio of Channels - Ustrips", apv_cnt), Form("c_ratio_chan_apv_%i", apv_cnt), 700, 500);
+		h_APV_ratio_ADCmax_chan_U[apv_cnt]->Draw();
+		h_APV_ratio_ADCmax_chan_U[apv_cnt]->SetTitle(Form("APV Channel Ratios - Run: %d, APV: %i)", runnum, apv_cnt));
+		h_APV_ratio_ADCmax_chan_U[apv_cnt]->GetXaxis()->SetTitle("ADC Ratio");
+		h_APV_ratio_ADCmax_chan_U[apv_cnt]->GetYaxis()->SetTitle("Entries");
+		h_APV_ratio_ADCmax_chan_U[apv_cnt]->SetMarkerStyle(2);
+		h_APV_ratio_ADCmax_chan_U[apv_cnt]->SetMarkerColor(06);
+		c_APV_ratio_ADCmax_chan_U[apv_cnt]->Update();
+
+		if(apv_cnt == first_apv){
+			c_APV_ratio_ADCmax_chan_U[apv_cnt]->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/APV_chan_ratios_U_strips_all_%i_ADCcut_%i.pdf(", runnum, ADCcut));
+		}
+		else if (apv_cnt > first_apv && apv_cnt < (last_apv-1)){
+			c_APV_ratio_ADCmax_chan_U[apv_cnt]->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/APV_ratios_U_strips_all_%i_ADCcut_%i.pdf", runnum, ADCcut));
+		}
+
+		if(apv_cnt == (last_apv - 1)){
+			c_APV_ratio_ADCmax_chan_U[apv_cnt]->Print(Form("/work/halla/sbs/jboyd/analysis/xtalk/plots/APV_ratios_U_strips_all_%i_ADCcut_%i.pdf)", runnum, ADC_cut));
+		}
+		c_APV_ratio_ADCmax_chan_U[apv_cnt]->Close();
+		gSystem->ProcessEvents();
+
+	}
+	//END OF APVs
+
+	
+
+}
+
+
+	
